7da623728fd071990e5fac538652f9df17a2a230
kaushikradheytv <kaushikradheytv@gmail.com>
2024-08-17 08:55:03 +0530

    Blaze Bringup

--- blaze_porsche.mk
+++ blaze_porsche.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/porsche/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_porsche
+PRODUCT_NAME := blaze_porsche
 PRODUCT_DEVICE := porsche
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
